c28814ae400c0ceb7b8cd4d6d18b793ba3dc1aa9
Mandeep Nehra <mandeep@Mandeeps-MacBook-Air.local>
2026-01-06 13:57:16 +0530

    testing

--- FundMe.sol
+++ FundMe.sol
@@ -3,21 +3,49 @@ pragma solidity ^0.8.19;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint;
-    uint public minUsd = 5e18;
+    uint public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping(address=> uint) public addressToAmountFunded;
 
-    function fund() public payable  {
-        
+    address public immutable i_owner;
 
-        require(msg.value.getConversionRate() > minUsd, "you dont have enough Eth to send");
+    constructor(){
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable  {
+    
+        require(msg.value.getConversionRate() > MINIMUM_USD, "you dont have enough Eth to send");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender]+msg.value;
-        
+    }
 
+    function withdraw() public onlyOwner{
+        for(uint funderIndex = 0; funderIndex<funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder]=0;
+        }
+        funders = new address[](0);
+        //transfer
+        payable(msg.sender).transfer(address(this).balance);
+        //send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess,"Send failed"); 
+        //call
+        (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value:address(this).balance}("");
+    }
+
+    modifier onlyOwner(){
+        //require(i_owner == msg.sender,"sender is not the owner!");
+        if(msg.sender != i_owner){
+            revert NotOwner();
+        }
+        _;
     }
 
     
